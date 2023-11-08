2f9febee21f4e712559f268cfd4a824e99a4e332
Tandra Ymir <tandraym@at-i3.msk.21-school.ru>
2023-11-08 14:21:32 +0300

    docker compose

--- spring_mvc_hibernate_iop/create_db.sql
+++ spring_mvc_hibernate_iop/create_db.sql
@@ -1,1 +1,14 @@
-CREATE DATABASE employee;
+CREATE TABLE employees (
+                           id SERIAL PRIMARY KEY NOT NULL ,
+                           name varchar(15),
+                           surname varchar(25),
+                           department varchar(20),
+                           salary int
+);
+
+INSERT INTO employees (name, surname, department, salary)
+VALUES
+    ('Zaur', 'Tregulov', 'IT', 500),
+    ('Oleg', 'Ivanov', 'Sales', 700),
+    ('Nina', 'Sidorova', 'HR', 850);
+
